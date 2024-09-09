11e23dde17f5714d556b0345b82869ef87d94b10
225Sumit <225sumitgupta@gmail.com>
2024-09-09 09:46:56 +0000

    class3 complete

--- dayOne/classTwo.js
+++ dayOne/classTwo.js
@@ -1,9 +1,11 @@
 // alert(3+3);
-console.log(3+3);
-console.log("sumit");
+// console.log(3+3);
+// console.log("sumit");
 let name ="Sumit"
 let age ="21"
 let isLoggedIn=true
 let city="kanpur"
 let mobileNo=7084876472
-console.table([name,age,isLoggedIn,city,mobileNo])
+// console.table([name,age,isLoggedIn,city,mobileNo])
+console.log(typeof age);
+console.log(typeof mobileNo);
---
+++ dayOne/class_3.js
@@ -0,0 +1,36 @@
+let name="sumit";
+// console.log(typeof name);
+
+let changeNumber=Number(name)
+// console.log(typeof changeNumber);
+// console.log(changeNumber);
+
+// *************************
+// oprations
+// *************************
+
+let value=10
+let negVal= -value;
+// console.log(negVal);
+// console.log(value+value);
+// console.log(value-value);
+// console.log(value*value);
+// console.log(value/value);
+// console.log(value%value);
+
+let str1="hello "
+let str2=" sumit"
+let str3=str1+  str2;
+// console.log(str3)
+
+console.log("1" + 2  + 1);
+console.log(1 + "2" + 1);
+console.log("1" + 2 + 2);
+console.log(1+2+"2");
+
+
+
+
+
+
+
