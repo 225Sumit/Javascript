1534a2e11f465f3081d82121521fcc4ab94f22e7
225Sumit <225sumitgupta@gmail.com>
2024-09-09 06:54:17 +0000

    day one add

---
+++ dayOne/classOne.js
@@ -0,0 +1,14 @@
+const accountId=1455566;
+let accountEmail = "Sumit@google.com";
+var accountPassword = "123";
+accountCity ="Jaipur";
+
+// accountId=2; const never change
+
+// console.log(accountId);
+
+accountEmail="Gupta@tesla.com"
+accountPassword="654321"
+accountCity="kanpur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ dayOne/classTwo.js
@@ -0,0 +1,9 @@
+// alert(3+3);
+console.log(3+3);
+console.log("sumit");
+let name ="Sumit"
+let age ="21"
+let isLoggedIn=true
+let city="kanpur"
+let mobileNo=7084876472
+console.table([name,age,isLoggedIn,city,mobileNo])
