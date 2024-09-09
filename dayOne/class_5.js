2385f1c218b2271b2d17bbbcdd85dd2f9a3bc113
225Sumit <225sumitgupta@gmail.com>
2024-09-09 11:03:21 +0000

    class 5

---
+++ dayOne/class_5.js
@@ -0,0 +1,35 @@
+const score=100
+const scoreValue=100.3
+const isLoggedln=false
+const ousideTemp=null
+let email;
+const id = Symbol('123')
+const anotherId= Symbol('456')
+const bigNum=123567n
+// console.table([score,scoreValue,isLoggedln,ousideTemp,email,id,anotherId,bigNum])
+// console.log(typeof score);
+// console.log(typeof scoreValue);
+// console.log(typeof isLoggedln);
+// console.log(typeof ousideTemp);
+// console.log(typeof email);
+// console.log(typeof id);
+// console.log(typeof anotherId);
+// console.log(typeof bigNum);
+
+// ***************
+// Function
+// ***************
+const heros=["shaktiman","naagraj","doga"]
+// console.log(heros);
+
+let myObj={
+    name:"sumit",
+    age:21,
+}
+console.log(myObj);
+
+const myFunction=function(){
+    console.log("New world");
+    
+}
+// myFunction();
