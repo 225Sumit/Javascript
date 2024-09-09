5211bdc455383e3660df18e998cb091111d82b24
225Sumit <225sumitgupta@gmail.com>
2024-09-09 18:45:01 +0000

    add class 6

---
+++ dayOne/class_6.js
@@ -0,0 +1,31 @@
+const name="sumit"
+const RollNo=60
+
+// console.log(`your name is ${name} and RollNo is ${RollNo}`);
+
+const gameLab= new String("Sumit-hp-com")
+// console.log(gameLab);
+// console.log(gameLab[0]);
+// console.log(gameLab.__proto__);
+// console.log(gameLab.length);
+
+gameHero="Sumit Gupta"
+// console.log(gameHero.toUpperCase());
+// console.log(gameHero.charAt(2));
+// console.log(gameHero.indexOf("i"));
+
+const Newhero = gameHero.substring(3,4)   //we never pass negative no
+// console.log(Newhero);
+
+// const OtherHero = gameHero.slice(3,4)  
+// const OtherHero = gameHero.slice(-5,8)      //we  pass negative no
+// console.log(OtherHero);
+
+HeroTrim="    Amit         "
+// console.log(HeroTrim);
+// console.log(HeroTrim.trim());
+// console.log(HeroTrim.toUpperCase().replace("A","SU").trim());
+
+star="hari-harish-durgesh-amit-sumit"
+// console.log(star.split("-"));
+
