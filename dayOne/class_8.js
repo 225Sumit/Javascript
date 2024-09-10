5f38272bc8ce8440a88c912a1797d4cfedfd06e1
225Sumit <225sumitgupta@gmail.com>
2024-09-10 18:05:49 +0000

    class_8 added

--- dayOne/class_8.js
+++ dayOne/class_8.js
@@ -1,2 +1,32 @@
-let myDate= new Date()
-console.log(myDate.toString());
+// let myDate= new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myDateNew= new Date(2023,0,23);
+// let myDateNew= new Date(2023,0,23,5,3);
+// let myDateNew= new Date("2023-01-14");
+
+// console.log(myDateNew.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myTimeStamp.toLocaleString());
+
+
+// let myDateNew= new Date("2023-01-14");
+// console.log(myDateNew.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let NewDateIs=new Date()
+// console.log(NewDateIs);
+// console.log(NewDateIs.getDay() );
+// console.log(NewDateIs.getMonth() );
+// console.log(NewDateIs.getHours() );
+// console.log(NewDateIs.getFullYear() );
+
+
+
+
