ac4f1ebe3fc32fd8659a51ce96fcd77d3dd3b51a
225Sumit <225sumitgupta@gmail.com>
2024-09-10 19:12:32 +0000

    class_1 (basics of Array)

---
+++ dayTwo/class_1.js
@@ -0,0 +1,55 @@
+// const myArr=[0,1,2,3,4,5,6,7,8,9]
+// console.log(myArr);
+
+// const myArr2= new Array(1,2,3,4)
+// console.log(myArr2[0]);
+// console.log(myArr2[3]);
+
+
+// console.log("orignal Array    " + myArr);
+// myArr.push(6)
+// console.log("after push   " + myArr);
+// console.log("orignal Array    " + myArr);
+// myArr.push(7)
+
+// console.log("after push   " + myArr);
+// myArr.pop()
+// console.log("after pop   " + myArr);
+// myArr.pop()
+// console.log("after pop   " + myArr);
+
+// const myArr=[0,1,2,3,4,5]
+// console.log("A         " + myArr);
+// myArr.unshift(9)
+// console.log("after unshift   " + myArr);
+// myArr.shift()
+// console.log("after shift   " + myArr);
+// myArr.shift()
+// console.log("after shift   " + myArr);
+// myArr.shift()
+// console.log("after shift   " + myArr);
+// myArr.shift()
+// console.log("after shift   " + myArr);
+
+// const myArr=[0,1,2,3,4,5]
+// console.log(myArr.includes(9));
+// console.log(myArr.includes(3));
+// console.log(myArr.indexOf(3));
+
+// const myArr=[0,1,2,3,4,5]
+// const NewArr = myArr.join()
+// console.log(NewArr);
+// console.log(typeof myArr);
+// console.log(typeof NewArr);
+
+
+const myArr=[0,1,2,3,4,5]
+console.log("A \t" + myArr);
+
+const newArr= myArr.slice(1,3)
+console.log("slice\t" + newArr);
+console.log("B \t" + myArr);
+
+const NewArr2 = myArr.splice(1,3)
+console.log("splice\t" + newArr);
+console.log("C \t" + myArr);
