835f7f35fa196972bf6ff1c7b044ed2d9003d7c3
225Sumit <225sumitgupta@gmail.com>
2024-09-10 06:43:57 +0000

    class_7 complete

---
+++ dayOne/class_7.js
@@ -0,0 +1,34 @@
+const score =400
+// console.log(score);
+
+const bal = new Number(100)
+// console.log(bal);
+// console.log(bal.toString().length);
+// console.log(bal.toFixed(1));
+
+const num=123.890
+// console.log(num.toPrecision(2));
+
+const change=10000000000
+// console.log(change.toLocaleString("en-IN"));
+
+
+// *************maths************
+// console.log(Math.abs(-78));
+// console.log(Math.round(3.8));
+// console.log(Math.ceil(3.8));
+// console.log(Math.floor(3.8));
+// console.log(Math.min(3.8,67,87,2,12));
+// console.log(Math.max(33.8,617,871,21,112));
+// console.log(Math.random());
+// console.log((Math.random()*10));
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+max));
+
+
+
+
