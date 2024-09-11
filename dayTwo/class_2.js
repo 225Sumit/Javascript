322b00b66723f85df40e99d95b3757dc3b1c085d
225Sumit <225sumitgupta@gmail.com>
2024-09-11 10:06:38 +0000

    class_2 add(some array method are using)

---
+++ dayTwo/class_2.js
@@ -0,0 +1,33 @@
+const marvel_heros=["thor","ironman","spiderman"];
+const dc_heros=["superman","batman"]
+
+// push mathod
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+// concat mathad
+// const all= marvel_heros.concat(dc_heros)      
+// console.log(all);
+
+// spred mathod
+const all=[...marvel_heros,...dc_heros]
+// console.log(all);
+
+// const anArr =[[1,2],[3,[4,[5,6]],[5,6]]];
+// console.log(anArr);
+// const newarr=anArr.flat(Infinity)
+// console.log(newarr);
+
+// console.log(Array.isArray("sumit"));
+// console.log(Array.from("sumit"));
+
+let arr1=100
+let arr2=200
+let arr3=300
+console.log(Array.of(arr1,arr2,arr3));
+
+
+
+
+
+
