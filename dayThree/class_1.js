724ffb5ac3e12ace7f1a746791d12218e49609b9
225Sumit <225sumitgupta@gmail.com>
2024-09-12 20:20:28 +0000

    funcation and parameters

---
+++ dayThree/class_1.js
@@ -0,0 +1,35 @@
+// function myName() {
+//     console.log("S");
+//     console.log("U");
+//     console.log("M");
+//     console.log("I");
+//     console.log("T");
+//     console.log("G");
+//     console.log("U");
+//     console.log("P");
+//     console.log("T");
+//     console.log("A");
+// }
+// myName()
+
+function add(num1,num2) {
+    console.log(num1 + num2);
+}
+// add(3,9)
+// add(3,"a")
+
+
+// function add(num1,num2) {
+//     let result = num1 + num2
+//     return result
+// }
+// result=add(12,23)
+// console.log("Result:",result);
+
+
+function loginUser(username){
+    console.log(`${username} just logged In`);
+    return
+     
+}
+loginUser("sumit")
