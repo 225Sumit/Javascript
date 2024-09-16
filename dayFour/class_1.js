371de92babdcc5b67148f92fcd106a36a75a743e
225Sumit <225sumitgupta@gmail.com>
2024-09-16 11:02:17 +0000

    if else add

---
+++ dayFour/class_1.js
@@ -0,0 +1,43 @@
+// const tem = 40
+// if (tem === 40)
+// {
+//     console.log(" equal to the 40");
+    
+// }else{
+//     console.log("not equal to the 40");
+    
+// }
+
+// const score = 200
+// if (score>100) {
+//     var power="fly"
+//     console.log(`user power :${power}`);
+// }
+
+// const bal =1000;
+// if(bal<500){
+//     console.log("less than 500");
+// }else if(bal<750){
+//     console.log("less than 750");
+// }else if(bal<900){
+//     console.log("less than 900");
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn=true
+const debitCard=true
+const userLoggedInFromGoogle=false
+const email=true
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy course");
+    
+// }
+
+
+if(userLoggedInFromGoogle||email){
+    console.log("user logged in");
+    
+}
