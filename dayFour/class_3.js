6bc422e1fafdacf151a660b0d6e434ae48d940cb
225Sumit <225sumitgupta@gmail.com>
2024-09-17 08:27:37 +0000

    class3 add

--- dayFour/class_3.js
+++ dayFour/class_3.js
@@ -7,11 +7,23 @@ const userEmail=[]
     
 // }
 
-if(userEmail.length===0){
-    console.log("Array is empty");
-}
-const emptyObj={}
-if(Object.keys(emptyObj).length===0){
-    console.log("object is empty");
+// if(userEmail.length===0){
+//     console.log("Array is empty");
+// }
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("object is empty");
     
-}
+// }
+
+// let val1;
+// val1 = 5??10
+// val1=null??10
+// val1=undefined??15
+// val1=null??10??20
+// console.log(val1);
+
+const iceTea=100
+iceTea<=80?console.log("less than 80"):console.log("more than 80")
+
+
