0cb6b940087ccad126b1b38616d53eaaf7cd834f
225Sumit <225sumitgupta@gmail.com>
2024-09-17 07:09:28 +0000

    add class 3

---
+++ dayFour/class_3.js
@@ -0,0 +1,17 @@
+const userEmail=[]
+// if(userEmail){
+//     console.log("got user email");
+    
+// }else{
+//     console.log("Don't have user email");
+    
+// }
+
+if(userEmail.length===0){
+    console.log("Array is empty");
+}
+const emptyObj={}
+if(Object.keys(emptyObj).length===0){
+    console.log("object is empty");
+    
+}
