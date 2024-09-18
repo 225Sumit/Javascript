23d07623d0e0057822ce3efa965bdc0e17075084
225Sumit <225sumitgupta@gmail.com>
2024-09-18 17:55:40 +0000

    reduce use

---
+++ dayFive/class_8.js
@@ -0,0 +1,23 @@
+// const myNum=[1,2,3]
+// const total=myNum.reduce(function(acc,nex){
+//     console.log(`acc ${acc} and nex value is ${nex}`);
+//     return acc + nex;
+    
+// },0)
+
+// console.log(total);
+
+
+
+// const myNum=[1,2,3]
+// const myTotal=myNum.reduce((acc,nex)=>acc+nex,0)
+// console.log(myTotal);
+
+
+const shoppingcart=[{itemName:'js course',price:2999},
+    {itemName:'py course',price:1999},
+    {itemName:'java course',price:3999},
+    {itemName:'rube course',price:6999},
+]
+const priceToPay=shoppingcart.reduce((acc,nex)=>acc+nex.price,0)
+console.log(priceToPay);
