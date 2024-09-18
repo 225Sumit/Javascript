0da4c74cc97391dfc45505a243987b658d5da10e
225Sumit <225sumitgupta@gmail.com>
2024-09-18 14:15:17 +0000

    for each loop

---
+++ dayFive/class_5.js
@@ -0,0 +1,40 @@
+const coding=["js","ruby","java","python","cpp"]
+// coding.forEach(function(item){
+//     console.log(item);
+    
+// })
+
+// coding.forEach((val) => {
+//     console.log(val);
+    
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach(printme)
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);
+    
+// })
+
+// const myCoding=[{
+//     lagName:"javascript",
+//     lagFile:"js"
+// },
+// {
+//     lagName:"java",
+//     lagFile:"java"
+// },
+// {
+//     lagName:"python",
+//     lagFile:"py"
+// },
+// ]
+
+// myCoding.forEach((item)=>{
+//     console.log(item.lagName);
+//     console.log(item.lagFile);
+    
+// })
