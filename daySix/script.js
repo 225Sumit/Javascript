2ce58122f43ecd332c78ed46e58bc2052fd95889
225Sumit <225sumitgupta@gmail.com>
2024-09-20 19:17:51 +0000

    add java script with selecters

--- daySix/script.js
+++ daySix/script.js
@@ -1,4 +1,24 @@
-const tile=document.getElementById("#title")
-tile.style.backgroundColor="red"
+// const dom= document.getElementById("title")
+// dom.style.backgroundColor="green"
+// dom.style.color="red"
+// dom.style.padding="30px"
+// dom.style.borderRadius="50px"
+// dom.innerHTML="<h1>sumit</h1>"
+// dom.setAttribute("class","scriptnew")
+
+
+const lorem=document.querySelector("#newh")
+lorem.style.backgroundColor="red"
+
+
+const lore=document.querySelectorall("li")
+// const nl= lore.querySelectorall("li")
+lore[1].style.backgroundColor="red"
+
+
+
+
+
+
 
 
